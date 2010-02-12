2b30fce2e263210f924bdf6ee1950381ee72018d
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2010-02-12 03:54:18 +0000

    Tagging PyUtilib 3.1651

---
+++ pyutilib/tags/3.1651/setup.py
@@ -0,0 +1,66 @@
+#  _________________________________________________________________________
+#
+#  PyUtilib: A Python utility library.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  _________________________________________________________________________
+
+"""
+Setup for PyUtilib package
+"""
+
+import os
+from setuptools import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(name="PyUtilib",
+    version='3.1651',
+    maintainer='William E. Hart',
+    maintainer_email='wehart@sandia.gov',
+    url = 'https://software.sandia.gov/trac/pyutilib.ply',
+    license = 'BSD',
+    platforms = ["any"],
+    description = 'PyUtilib: A collection of Python utilities',
+    long_description = read('README.txt'),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Unix Shell',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+      packages=['pyutilib'],
+      keywords=['utility'],
+      namespace_packages=['pyutilib'],
+      install_requires=[
+            'pyutilib.common',
+            'pyutilib.component.app', 
+            'pyutilib.component.config',
+            'pyutilib.component.core',
+            'pyutilib.component.executables',
+            'pyutilib.component.loader',
+            'pyutilib.dev',
+            'pyutilib.enum',
+            'pyutilib.excel',
+            'pyutilib.math',
+            'pyutilib.misc',
+            'pyutilib.ply',
+            'pyutilib.pyro',
+            'pyutilib.R',
+            'pyutilib.services',
+            'pyutilib.subprocess',
+            'pyutilib.th',
+            'pyutilib.virtualenv'
+      ]
+      )
+
