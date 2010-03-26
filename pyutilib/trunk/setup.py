f24698e27b713c6d03f265efcaedf8d5594cbc93
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2010-03-26 04:01:19 +0000

    Misc update to version #.

--- pyutilib/trunk/setup.py
+++ pyutilib/trunk/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 
 setup(name="PyUtilib",
-    version='3.1734',
+    version='3.1735',
     maintainer='William E. Hart',
     maintainer_email='wehart@sandia.gov',
     url = 'https://software.sandia.gov/trac/pyutilib.ply',
