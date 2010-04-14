618c01b0a8fe748831baccab3e709a25278f4dc0
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2010-04-14 23:35:23 +0000

    Tagging PyUtilib 3.0.1745.
    Deleting PyUtilib 3.0.1741, which couldn't be loaded due
    to a bug in pyutilib.component.doc.

--- pyutilib/tags/3.0.1745/setup.py
+++ pyutilib/tags/3.0.1745/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 
 setup(name="PyUtilib",
-    version='3.0.1741',
+    version='3.0.1745',
     maintainer='William E. Hart',
     maintainer_email='wehart@sandia.gov',
     url = 'https://software.sandia.gov/trac/pyutilib.ply',
