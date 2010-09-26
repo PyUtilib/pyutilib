7d34af372f4ba210e90cd3261ac9bdc4c3d06cfa
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2010-09-26 15:58:50 +0000

    Tagging PyUtilib 3.2.2044

--- pyutilib/tags/3.2.2044/setup.py
+++ pyutilib/tags/3.2.2044/setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
 
 
 setup(name="PyUtilib",
-    version='3.3',
+    version='3.2.2044',
     maintainer='William E. Hart',
     maintainer_email='wehart@sandia.gov',
     url = 'https://software.sandia.gov/trac/pyutilib.ply',
