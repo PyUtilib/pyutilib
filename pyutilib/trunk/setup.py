e3f20119a8d2956baa55c0255f047130a04351f1
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2012-04-28 21:45:48 +0000

    Setting up requirements differently for Python 3.x

--- pyutilib/trunk/setup.py
+++ pyutilib/trunk/setup.py
@@ -11,6 +11,7 @@
 Setup for PyUtilib package
 """
 
+import sys
 import os
 from setuptools import setup
 
@@ -18,6 +19,34 @@ from setuptools import setup
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+requires=[
+            'pyutilib.autotest>=2.0',
+            'pyutilib.common>=3.0.7',
+            'pyutilib.component.app>=3.2', 
+            'pyutilib.component.config>=3.4',
+            'pyutilib.component.core>=4.5.2',
+            'pyutilib.component.doc>=1.0.1',
+            'pyutilib.component.executables>=3.5',
+            'pyutilib.component.loader>=3.4',
+            'pyutilib.dev>=2.0',
+            'pyutilib.enum>=1.1',
+            'pyutilib.excel>=3.1',
+            'pyutilib.math>=3.3',
+            'pyutilib.misc>=5.3.1',
+            'pyutilib.ply>=3.0.6',
+            'pyutilib.pyro>=3.5',
+            'pyutilib.R>=3.1',
+            'pyutilib.services>=3.4',
+            'pyutilib.subprocess>=3.5.2',
+            'pyutilib.svn>=1.3',
+            'pyutilib.th>=5.3',
+            'pyutilib.virtualenv>=3.0',
+            'pyutilib.workflow>=3.2',
+            'nose'
+      ]
+if sys.version_info < (3,0):
+    requires.append('argparse')
+    requires.append('unittest2')
 
 setup(name="PyUtilib",
     version='4.1',
@@ -42,32 +71,6 @@ setup(name="PyUtilib",
       packages=['pyutilib'],
       keywords=['utility'],
       namespace_packages=['pyutilib'],
-      install_requires=[
-            'pyutilib.autotest>=2.0',
-            'pyutilib.common>=3.0.7',
-            'pyutilib.component.app>=3.2', 
-            'pyutilib.component.config>=3.4',
-            'pyutilib.component.core>=4.5.2',
-            'pyutilib.component.doc>=1.0.1',
-            'pyutilib.component.executables>=3.5',
-            'pyutilib.component.loader>=3.4',
-            'pyutilib.dev>=2.0',
-            'pyutilib.enum>=1.1',
-            'pyutilib.excel>=3.1',
-            'pyutilib.math>=3.3',
-            'pyutilib.misc>=5.3.1',
-            'pyutilib.ply>=3.0.6',
-            'pyutilib.pyro>=3.5',
-            'pyutilib.R>=3.1',
-            'pyutilib.services>=3.4',
-            'pyutilib.subprocess>=3.5.2',
-            'pyutilib.svn>=1.3',
-            'pyutilib.th>=5.3',
-            'pyutilib.virtualenv>=3.0',
-            'pyutilib.workflow>=3.2',
-            'argparse',
-            'nose',
-            'unittest2'
-      ]
+      install_requires=requires
       )
 
