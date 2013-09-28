88901e83321407d5172c9f0de0435f9268bf2188
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2013-09-28 01:58:59 +0000

    Tagging PyUtilib 4.6.3215

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if sys.version_info < (2,7):
     requires.append('unittest2')
 
 setup(name="PyUtilib",
-    version='4.7',
+    version='4.6.3215',
     maintainer='William E. Hart',
     maintainer_email='wehart@sandia.gov',
     url = 'https://software.sandia.gov/trac/pyutilib',
