a95b99a73376095d0ce320e84f9748f8c855f441
gahacke <gahacke@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2014-11-17 09:48:52 +0000

    Adding support for Pyro4. Defaults to Pyro3 if available

--- pyutilib/pyro/util.py
+++ pyutilib/pyro/util.py
@@ -7,16 +7,25 @@
 #  the U.S. Government retains certain rights in this software.
 #  _________________________________________________________________________
 
-__all__ = ['get_nameserver']
+__all__ = ['get_nameserver','shutdown_pyro_components']
 
 import os
+import sys
 import time
 import random
-import Pyro.core
-import Pyro.naming
-from Pyro.errors import NamingError, ConnectionDeniedError
+
+from pyutilib.pyro import using_pyro3, using_pyro4
+from pyutilib.pyro import Pyro as _pyro
+if sys.version_info >= (3,0):
+    xrange = range
+    import queue as Queue
+else:
+    import Queue
 
 def get_nameserver(host=None, num_retries=30):
+
+    if _pyro is None:
+        raise ImportError("Pyro or Pyro4 is not available")
 
     timeout_upper_bound = 5.0
 
@@ -25,20 +34,30 @@ def get_nameserver(host=None, num_retries=30):
     elif 'PYRO_NS_HOSTNAME' in os.environ:
         host = os.environ['PYRO_NS_HOSTNAME']
 
-    Pyro.core.initServer()
+    # Deprecated in Pyro3
+    # Removed in Pyro4
+    if using_pyro3:
+        _pyro.core.initServer()
 
     ns = None
 
+    if using_pyro3:
+        connection_problem = _pyro.errors.ConnectionDeniedError
+    else:
+        connection_problem = _pyro.errors.TimeoutError
     for i in xrange(0, num_retries):
         try:
-            if host is None:
-                ns=Pyro.naming.NameServerLocator().getNS()
+            if using_pyro3:
+                if host is None:
+                    ns = _pyro.naming.NameServerLocator().getNS()
+                else:
+                    ns = _pyro.naming.NameServerLocator().getNS(host)
             else:
-                ns=Pyro.naming.NameServerLocator().getNS(host)
+                ns = _pyro.locateNS(host=host)
             break
-        except NamingError:
+        except _pyro.errors.NamingError:
             pass
-        except ConnectionDeniedError:
+        except connection_problem:
             # this can occur if the server is too busy.
             pass
 
@@ -60,3 +79,44 @@ def get_nameserver(host=None, num_retries=30):
         raise SystemExit
 
     return ns
+
+#
+# a utility for shutting down Pyro-related components, which at the
+# moment is restricted to the name server and any dispatchers. the
+# mip servers will come down once their dispatcher is shut down.
+# NOTE: this is a utility that should eventually become part of
+#       pyutilib.pyro, but because is prototype, I'm keeping it
+#       here for now.
+#
+
+def shutdown_pyro_components(host=None, num_retries=30):
+
+    if _pyro is None:
+        raise ImportError("Pyro or Pyro4 is not available")
+
+    ns = get_nameserver(host=host, num_retries=num_retries)
+    if ns is None:
+        print("***WARNING - Could not locate name server "
+              "- Pyro PySP components will not be shut down")
+        return
+
+    if using_pyro3:
+        ns_entries = ns.flatlist()
+        for (name,uri) in ns_entries:
+            if name == ":Pyro.NameServer":
+                proxy = _pyro.core.getProxyForURI(uri)
+                proxy._shutdown()
+            elif name == ":PyUtilibServer.dispatcher":
+                proxy = _pyro.core.getProxyForURI(uri)
+                proxy.shutdown()
+    elif using_pyro4:
+        ns_entries = ns.list()
+        for name in ns_entries:
+            if name == ":PyUtilibServer.dispatcher":
+                URI = ns.lookup(name)
+                proxy = _pyro.Proxy(URI)
+                proxy.shutdown()
+                proxy._pyroRelease()
+        print("")
+        print("*** NameServer must be shutdown manually when using Pyro4 ***")
+        print("")
