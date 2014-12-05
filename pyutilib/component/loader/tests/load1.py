d6b80d5c96d84df4996e33f1c62ed5239975a5ba
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2014-12-05 16:34:27 +0000

    Adding JSON tests. Making YAML tests conditional on the installation of PyYaml

--- pyutilib/component/loader/tests/load1.py
+++ pyutilib/component/loader/tests/load1.py
@@ -4,10 +4,13 @@ import os
 import sys
 import logging
 
-currdir=sys.argv[-1]+os.sep
+currdir=sys.argv[-2]+os.sep
 
 logging.basicConfig(level=logging.DEBUG)
 pyutilib.component.core.PluginGlobals.get_env().load_services(path=currdir+"plugins1")
 pyutilib.misc.setup_redirect(currdir+"load1.out")
-pyutilib.component.core.PluginGlobals.pprint()
+if sys.argv[-1] == "json":
+    pyutilib.component.core.PluginGlobals.pprint(json=True)
+else:
+    pyutilib.component.core.PluginGlobals.pprint()
 pyutilib.misc.reset_redirect()
