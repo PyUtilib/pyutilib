96c2fe65de44be3fe76c604a141d79abcf340ebb
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2015-02-28 19:54:17 +0000

    Adding support for xlrd and openpyxl interfaces

--- pyutilib/excel/spreadsheet_openpyxl.py
+++ pyutilib/excel/spreadsheet_openpyxl.py
@@ -15,67 +15,47 @@ A class for interacting with an Excel spreadsheet.
 # Imports
 #
 import os
-
-#
-# Attempt to import win32com stuff.  If this fails, then
-# set a flag to tell the ExcelSpreadsheet class that it should
-# raise an exception in its constructor.
-#
-from win32com.client.dynamic import Dispatch
-from pythoncom import CoInitialize, CoUninitialize
-from pythoncom import CoInitialize, CoUninitialize, com_error
+import warnings
+import openpyxl
 
 from pyutilib.excel.base import ExcelSpreadsheet_base
 
 
-class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
-
-    xlToLeft = 1
-    xlToRight = 2
-    xlUp = 3
-    xlDown = 4
-    xlThick = 4
-    xlThin = 2
-    xlEdgeBottom=9
-
-    _excel_app_ptr = None
-    _excel_app_ctr = 0
+class ExcelSpreadsheet_openpyxl(ExcelSpreadsheet_base):
 
     def can_read(self): return True
     def can_write(self): return True
-    def can_calculate(self): return True
+    def can_calculate(self): return False
 
-    def __init__(self,filename=None, worksheets=(1,), default_worksheet=1):
+    def __init__(self, filename=None, worksheets=(1,), default_worksheet=1):
         """
         Constructor.
         """
-        self.xl=None
-        self.xlsfile=None
+        self.wb = None
+        self.xlsfile = None
+        self._ws = {}
         if filename is not None:
             self.open(filename, worksheets, default_worksheet)
 
-    def open(self,filename,worksheets=(1,),default_worksheet=1):
+    def open(self, filename, worksheets=(1,), default_worksheet=1):
         """
         Initialize this object from a file.
         """
         #
         # Set the excel spreadsheet name
         #
-        if filename[1] == ":":
-            self.xlsfile=filename
-        else:
-            self.xlsfile=os.getcwd() + "\\"+filename
+        self.xlsfile = filename
         #
         # Start the excel spreadsheet
         #
-        self.xl = self._excel_dispatch()
-        self.wb = self.xl.Workbooks.Open(self.xlsfile)
-        self.worksheets=set(worksheets)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.wb = openpyxl.load_workbook(self.xlsfile)
+        self.worksheets = self.wb.get_sheet_names()
         self._ws = {}
         for wsid in worksheets:
-            self._ws[wsid] = self.wb.Worksheets.Item(wsid)
-            self._ws[wsid].Activate()
-        self.default_worksheet=default_worksheet
+            self._ws[wsid] = self.wb.get_sheet_by_name( self.worksheets[wsid-1] )
+        self.default_worksheet = default_worksheet
 
 
     def ws(self):
@@ -89,14 +69,20 @@ class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
         self.close()
 
     def activate(self, name):
-        """ Activate a specific sheet """
+        """
+        Activate a specific sheet
+        """
         if name is None:
             return
-        if not name in self._ws:
-            self.worksheets.add(name)
-            self._ws[name] = self.wb.Worksheets.Item(name)
-            self._ws[name].Activate()
-        self.default_worksheet=name
+        if name in self.worksheets:
+            idx = self.worksheets.index(name)
+            self.wb.active = idx
+        elif not name in self._ws:
+            self.wb.add_sheet(name)
+            idx = len(self.wb.get_sheet_names())-1
+            self._ws[idx] = self.wb.Worksheets.Item(name)
+            self._ws[idx].Activate()
+        self.default_worksheet=idx
 
     def close(self):
         """
@@ -104,11 +90,9 @@ class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
         """
         if self is None:       #pragma:nocover
             return
-        if self.xl is None:
+        if self._ws is None:
             return
-        if self.xl.ActiveWorkbook is not None:
-            self.xl.ActiveWorkbook.Close(SaveChanges=0)
-        self._excel_quit()
+        self._ws = {}
 
     def calc_iterations(self, val=None):
         if val is None:
@@ -132,21 +116,39 @@ class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
 
     def set_array(self, row, col, val, wsid=None):
         self.activate(wsid)
-        nrows=len(val)
-        return self.set_range( (self.ws().Cells(row,col), self.ws().Cells(row+nrows-1,col+1)), val)
+        ws = self.wb.active
+        xlo=row
+        xhi=row+len(val)-1
+        ylo=col
+        yhi=col+1
+        for row in range(xhi-xlo+1):
+            for col in range(yhi-ylo+1):
+                #print(row,col)
+                ws.cell(row=xlo+row, column=ylo+col).value = val[row][col]
 
     def get_array(self, row, col, row2, col2, wsid=None, raw=False):
-        return self.get_range( (self.ws().Cells(row,col), self.ws().Cells(row2,col2)), wsid, raw)
+        self.activate(wsid)
+        ws = self.wb.active
+        ans = []
+        for row in ws.get_squared_range(row,col,row2,col2):
+            ans_ = []
+            for col in row:
+                ans_.append( col.value )
+            if len(ans_) == 1:
+                ans.append(ans_[0])
+            else:
+                ans.append(tuple(ans_))
+        return tuple(ans)
 
     def set_range(self, rangename, val, wsid=None):
         """
         Set a range with a given value (or set of values)
         """
-        self.activate(wsid)
+        #
+        # Process data into tuples
+        #
         if type(val) in (int,float):
             val=((val,),)
-        if len(val) != self.get_range_nrows(rangename):
-            raise IOError("Setting data with "+str(len(val))+" rows but range has "+str(self.get_range_nrows(rangename)))
         if type(val) is tuple:
             data = val
         elif type(val) not in (float,int,bool):
@@ -159,7 +161,19 @@ class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
                 else:
                     data.append(tuple(item))
             data = tuple(data)
-        self._range(rangename).Value = data
+        #
+        # Set the data
+        #
+        self.activate(wsid)
+        _range = self._range(rangename)
+        if not _range is None and len(val) != self.get_range_nrows(rangename):
+            raise IOError("Setting data with "+str(len(val))+" rows but range has "+str(self.get_range_nrows(rangename)))
+        ws = _range.destinations[0][0]
+        #
+        ylo,xlo,yhi,xhi = openpyxl.worksheet.worksheet.range_boundaries(_range.destinations[0][1])
+        for row in range(xhi-xlo+1):
+            for col in range(yhi-ylo+1):
+                ws.cell(row=xlo+row, column=ylo+col).value = data[row][col]
 
     def get_column(self, colname, wsid=None, raw=False, contiguous=False):
         """
@@ -190,98 +204,65 @@ class ExcelSpreadsheet_win32com(ExcelSpreadsheet_base):
         Get values for a specified range
         """
         self.activate(wsid)
-        range = self._range(rangename)
-        return self._get_range_data(range, raw)
+        _range = self._range(rangename)
+        data = self._get_range_data(_range, raw)
+        if len(data) == 1:
+            return data[0]
+        return data
 
-    def _get_range_data(self, range, raw):
-        if raw:
-            return range.Value
-        nrows = range.Rows.Count
-        ncols = range.Columns.Count
-        if range.Columns.Count == 1:
-            if nrows == 1:
-                #
-                # The range is a singleton, so return a float
-                #
-                return range.Value
+    def _get_range_data(self, _range, raw):
+        ans = []
+        for row in _range.destinations[0][0].iter_rows(_range.destinations[0][1]):
+            rvals = []
+            for cell in row:
+                rvals.append( cell.value )
+            if len(rvals) == 1:
+                ans.append(rvals[0])
             else:
-                #
-                # The range is a column of data, so return a tuple of floats
-                #
-                ans=[]
-                for val in range.Value:
-                    ans.append(val[0])
-                return tuple(ans)
-        else:
-            if nrows == 1:
-                #
-                # The range is a row of data, so return a tuple of floats
-                #
-                return range.Value[0]
-            else:
-                #
-                # The range is a two-dimensional array, so return the values
-                # as a tuple of tuples.
-                #
-                return range.Value
+                ans.append(tuple(rvals))
+        return tuple(ans)
 
     def get_range_nrows(self, rangename, wsid=None):
         """
         Get the number of rows in a specified range
         """
         self.activate(wsid)
-        return self._range(rangename).Rows.Count
+        _range = self._range(rangename)
+        data = self._get_range_data(_range, False)
+        return len(data)
 
     def get_range_ncolumns(self, rangename, wsid=None):
         """
         Get the number of columns in a specified range
         """
         self.activate(wsid)
-        return self._range(rangename).Columns.Count
+        _range = self._range(rangename)
+        data = self._get_range_data(_range, False)
+        if type(data[0]) is tuple:
+            return len(data[0])
+        return 1
 
-    def _range(self, rangeid, wsid=None):
+    def _range(self, rangeid, wsid=None, exception=True):
         """
         Return a range for a given worksheet
         """
         self.activate(wsid)
         try:
             #
-            # If rangeid is a tuple, then this is a list of arguments to pass
-            # to the Range() method.
+            # If rangeid is a tuple, then this is a list of arguments 
             #
             if type(rangeid) is tuple:
-                return self.ws().Range(*rangeid)
+                return self.wb.get_squared_range(*rangeid)
             #
             # Otherwise, we assume that this is a range name.
             #
-            # to the Range() method.
-            #
-            else:
-                return self.ws().Range(rangeid)
-        except com_error:
+            _range = self.wb.get_named_range(rangeid)
+            if _range is None:
+                if ':' in rangeid:
+                    return openpyxl.workbook.names.named_range.NamedRange("temp", [(self.wb.active, rangeid)], None)
+                if exception:
+                    raise IOError("Unknown range name `"+str(rangeid)+"'")
+            return _range
+        except openpyxl.exceptions.NamedRangeException:
             raise IOError("Unknown range name `"+str(rangeid)+"'")
 
-    def _excel_dispatch(self):
-        """
-        A private method that launches Excel.
-        This keeps a counter of the number of ExcelSpreadsheet objects
-        that are running.
-        """
-        if ExcelSpreadsheet._excel_app_ctr == 0:
-            ExcelSpreadsheet._excel_app_ptr = Dispatch('Excel.Application')
-        ExcelSpreadsheet._excel_app_ctr += 1
-        return ExcelSpreadsheet._excel_app_ptr
-
-    def _excel_quit(self):
-        """
-        A method that quits from Excel after all spreadsheets are closed.
-        """
-        if ExcelSpreadsheet._excel_app_ctr == 0:
-            return None
-        if ExcelSpreadsheet._excel_app_ctr > 1:
-            ExcelSpreadsheet._excel_app_ctr -= 1
-            return ExcelSpreadsheet._excel_app_ctr
-        ExcelSpreadsheet._excel_app_ctr = 0
-        ExcelSpreadsheet._excel_app_ptr.Quit()
-        del ExcelSpreadsheet._excel_app_ptr
-        return 0
