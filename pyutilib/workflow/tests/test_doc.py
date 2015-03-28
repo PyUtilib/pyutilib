2a0a70d860aaedebd9804c096d73f7efa461f434
gahacke <gahacke@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2015-03-28 21:36:52 +0000

    reverting r3517, moving the hack into pyomo

--- pyutilib/workflow/tests/test_doc.py
+++ pyutilib/workflow/tests/test_doc.py
@@ -8,12 +8,11 @@ import pyutilib.th as unittest
 currdir = dirname(abspath(__file__))+os.sep
 datadir = os.sep.join([dirname(dirname(dirname(dirname(abspath(__file__))))),'doc','workflow','examples'])+os.sep
 
-os.chdir(datadir)
 sys.path.insert(0, datadir)
-
-from test_example import *
-
-sys.path.remove(datadir)
+try:
+    from test_example import *
+finally:
+    sys.path.remove(datadir)
 
 # Execute the tests
 if __name__ == '__main__':
