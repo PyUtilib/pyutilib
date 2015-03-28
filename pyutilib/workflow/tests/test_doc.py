4a29f202062a49705285c3100db04bec1e431a35
gahacke <gahacke@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2015-03-28 21:37:57 +0000

    reverting change that did not belong in last commit

--- pyutilib/workflow/tests/test_doc.py
+++ pyutilib/workflow/tests/test_doc.py
@@ -8,11 +8,12 @@ import pyutilib.th as unittest
 currdir = dirname(abspath(__file__))+os.sep
 datadir = os.sep.join([dirname(dirname(dirname(dirname(abspath(__file__))))),'doc','workflow','examples'])+os.sep
 
+os.chdir(datadir)
 sys.path.insert(0, datadir)
-try:
-    from test_example import *
-finally:
-    sys.path.remove(datadir)
+
+from test_example import *
+
+sys.path.remove(datadir)
 
 # Execute the tests
 if __name__ == '__main__':
