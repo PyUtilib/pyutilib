03e448639d6aab09405d615cfaa91414f01f5115
gahacke <gahacke@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2015-09-15 06:04:49 +0000

    fixing syntax error

--- pyutilib/pyro/worker.py
+++ pyutilib/pyro/worker.py
@@ -319,7 +319,7 @@ class MultiTaskWorker(TaskWorkerBase):
                         type_results = results[type_name] = []
                         for task in sorted(type_tasks, key=lambda x: x['id']):
                             task['result'] = self.process(task['data'])
-                            if self._worker_error
+                            if self._worker_error:
                                 task['processedBy'] = self.WORKERNAME
                                 type_results.append(task)
                                 print("Task worker reported error during processing "
