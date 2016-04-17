aa621095ab6efd02f9c496d06c12ce64e87d2701
Hart <whart222@gmail.com>
2016-04-17 08:26:52 -0400

    Bug fix

--- install_jython.sh
+++ install_jython.sh
@@ -14,14 +14,13 @@ NON_GROUP_ID=${JYTHON#*:}
 _JYTHON_BASENAME=${NON_GROUP_ID/:/-}
 export CLASSPATH=$VIRTUAL_ENV/javalib/*
 java -jar $VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
-ls $HOME/jython/bin
 
 #
 # Create virtual environment
 #
 # pip isn't available in 2.7.0, so we need to use an older version of virtualenv
 #
-pip install virtualenv=1.9.1
+pip install virtualenv==1.9.1
 virtualenv --version
 virtualenv -p $HOME/jython/bin/jython $HOME/myvirtualenv
 
@@ -29,4 +28,4 @@ virtualenv -p $HOME/jython/bin/jython $HOME/myvirtualenv
 # Install packages into the jython virtual environment
 #
 export PATH=$HOME/myvirtualenv/bin:$PATH
-export PYTHON=$HOME/myvirtualenv/bin/python
+ls $HOME/myvirtualenv/bin
