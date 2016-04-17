f5ce13f9801d33265400ffcfd70443206863a556
Hart <whart222@gmail.com>
2016-04-17 07:53:04 -0400

    Misc edits
    Removed license info.  IMO, the script is sufficiently changed that
    it can be considered not a derivative of the original that I was
    working off of.

--- install_jython.sh
+++ install_jython.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# Copyright 2014 Bastian Bowe
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set -e
 set -v
@@ -19,38 +6,23 @@ set -v
 pip install jip
 jip install $JYTHON
 #jip install joda-time:joda-time:2.3
+
+#
+# Install Jython
+#
 NON_GROUP_ID=${JYTHON#*:}
 _JYTHON_BASENAME=${NON_GROUP_ID/:/-}
-OLD_VIRTUAL_ENV=$VIRTUAL_ENV
-java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
+export CLASSPATH=$VIRTUAL_ENV/javalib/*
+java -jar $VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
 
-BEFORE_PY_26=$($HOME/jython/bin/jython -c "import sys; print sys.version_info < (2, 6)")
-if [ "$BEFORE_PY_26" == "True" ]
-then
-    # Travis CI virtualenv version is greater 1.9.1, which was the
-    # last version compatible with Python < 2.6
-    pip install virtualenv==1.9.1
-fi
-
+#
+# Create virtual environment
+#
 virtualenv --version
-# --distribute is a workaround as setuptools don't install on Jython properly
-virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv
-
-if [ "$BEFORE_PY_26" == "True" ]
-then
-    # No SSL support for Jython
-    cat > $HOME/myvirtualenv/pip.conf <<EOF
-[install]
-insecure = true
-EOF
-    cat <<EOF >> $HOME/myvirtualenv/bin/activate
-export PIP_CONFIG_FILE=$HOME/myvirtualenv/pip.conf
-EOF
-fi
+virtualenv -p $HOME/jython/bin/jython $HOME/myvirtualenv
 
 #
 # Install packages into the jython virtual environment
 #
 export PATH=$HOME/myvirtualenv/bin:$PATH
 export PYTHON=$HOME/myvirtualenv/bin/python
-export CLASSPATH=$VIRTUAL_ENV/javalib/*
