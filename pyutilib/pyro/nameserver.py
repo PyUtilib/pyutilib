f3e516b3d2b892707f9dd5760826e0d91513d2a1
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-11-14 09:34:01 -0700

    Updating default behavior when starting up nameserver. Using the Pyro3 default settings (default host is not localhost)

--- pyutilib/pyro/nameserver.py
+++ pyutilib/pyro/nameserver.py
@@ -10,13 +10,23 @@
 __all__ = ('start_ns', 'start_nsc')
 
 import sys
+import socket
 from pyutilib.pyro.util import set_maxconnections, Pyro, using_pyro4
 
 
 def start_ns(max_allowed_connections=None):
     if Pyro is not None:
         set_maxconnections(max_allowed_connections=max_allowed_connections)
-        Pyro.naming.main(sys.argv[1:])
+        args = []
+        for val in sys.argv[1:]:
+            if val.startswith("-n") or val.startswith("--host"):
+                args = list(sys.argv[1:])
+                break
+        else: # execute when no break occurs
+            args = list(sys.argv[1:])
+            args.append("-n")
+            args.append(str(socket.gethostname()))
+        Pyro.naming.main(args)
     else:
         raise ImportError("Pyro or Pyro4 is not installed")
 
