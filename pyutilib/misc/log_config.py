2a25e4c90e154b9f424c2d1baee5e1546bbd5c56
John Siirola <(none)>
2017-05-14 11:42:44 -0600

    Cleaning up the LogHandler
    * Improving the general efficiency of the LogHandler.
    * Replacing LogHandler.__init__(*args, **kwds) with named arguments
    * Improving the "base" substitution logic so that it only replaces the
      base if it occurs at the beginning of the path.
    * Adding the ability to specify an output stream other than stdout.
    * Adding unit tests

--- pyutilib/misc/log_config.py
+++ pyutilib/misc/log_config.py
@@ -7,59 +7,54 @@
 #  the U.S. Government retains certain rights in this software.
 #  _________________________________________________________________________
 
+import inspect
 import logging
-from pyutilib.misc.indent_io import StreamIndenter
+import os
+import sys
 
+from pyutilib.misc.indent_io import StreamIndenter
 
 class LogHandler(logging.Handler):
 
-    def __init__(self, base, *args, **kwargs):
-        if 'verbosity' in kwargs:
-            self.verbosity = kwargs.pop('verbosity')
+    def __init__( self, base='', stream=sys.stdout, 
+                  level=logging.NOTSET, verbosity=None ):
+        if verbosity is not None:
+            self.verbosity = verbosity
         else:
             self.verbosity = lambda: True
-        logging.Handler.__init__(self, *args, **kwargs)
+        logging.Handler.__init__(self, level=level)
 
+        self.stream = stream
         self.basepath = base
 
     def emit(self, record):
-        import sys  # why?  Isn't this imported above?
-        # Doesn't work w/o it though ... ?
-
         level = record.levelname
-        filename = record.pathname  # file path
-        lineno = record.lineno
         msg = record.getMessage()
-        try:
-            function = record.funcName
-        except AttributeError:
-            function = '(unknown)'
-
-        filename = filename.replace(self.basepath, '[base]')
 
         if self.verbosity():
-            sys.stdout.write('%(level)s: "%(fpath)s", %(lineno)d, '
-                             '%(caller)s\n' % {
-                                 'level': level,
-                                 'fpath': filename,
-                                 'lineno': lineno,
-                                 'caller': function.strip(),
-                             })
-            StreamIndenter(sys.stdout, "\t").write(msg.strip() + "\n")
+            filename = record.pathname  # file path
+            lineno = record.lineno
+            try:
+                function = record.funcName
+            except AttributeError:
+                function = '(unknown)'
+            if self.basepath and filename.startswith(self.basepath):
+                filename = '[base]' + filename[len(self.basepath):]
+
+            self.stream.write(
+                '%s: "%s", %d, %s\n' %
+                ( level, filename, lineno, function.strip(), ))
+            StreamIndenter(self.stream, "\t").write(msg.strip() + "\n")
         else:
-            lines = msg.splitlines(True)
-            sys.stdout.write('%(level)s: %(msg)s\n' % {
-                'level': level,
-                'msg': lines and lines.pop(0).strip() or '\n',
-            })
-            if lines:
-                StreamIndenter(sys.stdout, "\t").writelines(lines)
-                if len(lines[-1]) and lines[-1][-1] != '\n':
-                    sys.stdout.write('\n')
+            lines = msg.strip().splitlines(False)
+            self.stream.write('%s: %s\n' % (
+                level, '\n\t'.join(lines) ))
 
 # Set up default logging for PyUtilib
-from os.path import abspath, dirname, join, normpath
-pyutilib_base = normpath(join(dirname(abspath(__file__)), '..', '..', '..'))
+pyutilib_base = os.path.normpath(os.path.join(
+    os.path.dirname( os.path.abspath(
+        inspect.getframeinfo(inspect.currentframe()).filename)),
+    '..', '..', '..'))
 
 logger = logging.getLogger('pyutilib')
 logger.setLevel(logging.WARNING)
