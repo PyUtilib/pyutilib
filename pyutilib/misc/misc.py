a4518e26fa91ac86033d7e05c11f00b3bb1ef385
John Siirola <(none)>
2017-10-05 17:43:18 -0600

    Python 3.x fix for Windows

--- pyutilib/misc/misc.py
+++ pyutilib/misc/misc.py
@@ -447,11 +447,11 @@ def create_hardlink(src, dst):
     """
     Create a hard link where dst points to src.
     """
-    if os.name == 'nt':
-        # Windows
+    if os.name == 'nt' and sys.version_info[:2] < (3.2):
+        # Windows; Python added native support for hard links in 3.2
         import ctypes
         if not ctypes.windll.kernel32.CreateHardLinkA(dst, src, 0):
             raise OSError("Failed to create hard link for file %s" % (src))
     else:
-        # Unix
+        # Unix; Windows >= Python3.2
         os.link(src, dst)
