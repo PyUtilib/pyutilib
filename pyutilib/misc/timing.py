06bed0039cacfcdd6eab669cb14437787e08924b
Carl Laird <carldlaird@users.noreply.github.com>
2017-10-20 12:43:06 -0500

    moved decision about output to when the write is performed (instead of when the class is created), and added the ability to use a logger instead of an ostream

--- pyutilib/misc/timing.py
+++ pyutilib/misc/timing.py
@@ -19,18 +19,27 @@ _loadTime = time.time()
 class TicTocTimer(object):
     """A class to calculate and report elapsed time.
 
-    Args:
-        ostream (FILE): the output stream to print the timing
-            information.
-    """
-    def __init__(self, ostream=None):
-        if ostream is None:
-            self._ostream = sys.stdout
-        else:
-            self._ostream = ostream
-        self._lastTime = time.time()
+    Examples:
+       >>> from pyutilib.misc.timing import TicTocTimer
+       >>> timer = TicTocTimer()
+       >>> timer.tic('starting timer') # starts the elapsed time timer (from 0)
+       >>> # ... do task 1
+       >>> timer.toc('task 1') # prints the elapsed time for task 1
 
-    def tic(self, msg=None, ostream=None):
+    If no ostream or logger is provided, then output is printed to sys.stdout
+
+    Args:
+        ostream (FILE): an optional output stream to print the timing
+            information
+        logger (Logger): an optional output stream using the python
+           logging package. Note: timing logged using logger.info
+    """
+    def __init__(self, ostream=None, logger=None):
+        self._lastTime = time.time()
+        self._ostream = ostream
+        self._logger = logger
+
+    def tic(self, msg=None, ostream=None, logger=None):
         """Reset the tic/toc delta timer.
 
         This resets the reference time from which the next delta time is
@@ -41,17 +50,20 @@ class TicTocTimer(object):
                 (default), then prints out "Resetting the tic/toc delta
                 timer"; if it evaluates to :const:`False` (:const:`0`,
                 :const:`False`, :const:`""`) then no message is printed.
-            ostream (FILE): the output stream (overrides the ostream
+            ostream (FILE): an optional output stream (overrides the ostream
                 provided when the class was constructed).
+            logger (Logger): an optional output stream using the python
+                logging package (overrides the ostream provided when the
+                class was constructed). Note: timing logged using logger.info
         """
         self._lastTime = time.time()
         if msg is None:
             msg = "Resetting the tic/toc delta timer"
         if msg:
-            self.toc(msg=msg, delta=False, ostream=ostream)
+            self.toc(msg=msg, delta=False, ostream=ostream, logger=logger)
 
 
-    def toc(self, msg=None, delta=True, ostream=None):
+    def toc(self, msg=None, delta=True, ostream=None, logger=None):
         """Print out the elapsed time.
 
         This resets the reference time from which the next delta time is
@@ -67,8 +79,11 @@ class TicTocTimer(object):
                 the last call to :meth:`tic` or :meth:`toc`
                 (:const:`True` (default)) or since the module was first
                 loaded (:const:`False`).
-            ostream (FILE): the output stream (overrides the ostream
+            ostream (FILE): an optional output stream (overrides the ostream
                 provided when the class was constructed).
+            logger (Logger): an optional output stream using the python
+                logging package (overrides the ostream provided when the
+                class was constructed). Note: timing logged using logger.info
         """
         now = time.time()
         if delta:
@@ -83,10 +98,24 @@ class TicTocTimer(object):
         if msg:
             if ostream is None:
                 ostream = self._ostream
-            if delta:
-                ostream.write("[+%7.2f] %s\n" % (ans, msg))
-            else:
-                ostream.write("[%8.2f] %s\n" % (ans, msg))
+                if ostream is None and logger is None:
+                    ostream = sys.stdout
+                
+            if ostream is not None:
+                if delta:
+                    ostream.write("[+%7.2f] %s\n" % (ans, msg))
+                else:
+                    ostream.write("[%8.2f] %s\n" % (ans, msg))
+
+            if logger is None:
+                logger = self._logger
+
+            if logger is not None:
+                if delta:
+                    logger.info("[+%7.2f] %s\n" % (ans, msg))
+                else:
+                    logger.info("[%8.2f] %s\n" % (ans, msg))
+
         return ans
 
 _globalTimer = TicTocTimer()
