0d231d79b090f2cd9dfe582ff03873bd03ae12e3
William Hart <whart222@gmail.com>
2017-10-21 16:15:59 -0600

    Documentation edit

--- pyutilib/subprocess/tests/tee_script.py
+++ pyutilib/subprocess/tests/tee_script.py
@@ -3,7 +3,7 @@ import time
 # Write ERR first so that the output is deterministic
 sys.stderr.write("Tee Script: ERR\n")
 sys.stdout.write("Tee Script: OUT\n")
-# sleep just enough so tha the STDERR thread should finish flushing its
+# sleep just enough so that the STDERR thread should finish flushing its
 # buffer before the process terminates (at which point the order in
 # which the buffers are flushed is somewhat random).
 time.sleep(0.5)
