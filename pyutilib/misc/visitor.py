2ede416d2fdc3a91731c979f0836df126e6bd944
William Hart <whart222@gmail.com>
2017-11-27 10:11:24 -0700

    Adding documentation

--- pyutilib/misc/visitor.py
+++ pyutilib/misc/visitor.py
@@ -4,20 +4,72 @@ from collections import deque
 class SimpleVisitor(object):
 
     def visit(self, node):  #pragma: no cover
+        """
+	    Visit a node in a tree and perform some operation on
+	    it.
+
+        This method should be over-written by a user
+        that is creating a sub-class.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            nothing
+        """
         pass
 
     def children(self, node):
+        """
+        Return the children for a node in a tree.
+        
+        This method has a default implementation, but
+        this implementation will likely be redefined
+        to reflect the tree data structures used for 
+        a specific application.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            A list of children of the specified node.
+        """ 
         return node.children
     
     def is_leaf(self, node):
+        """
+        Return :const:`True` if the node has no children.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            :const:`True` if this node has no children, and :const:`False`
+            otherwise
+        """
         return len(node.children) == 0
 
     def finalize(self):     #pragma: no cover
+        """
+        Return the "final value" of the search.
+
+        The default implementation returns :const:`None`, because
+        the traditional visitor pattern does not return a value.
+
+        Returns:
+            The final value after the search.  Default is :const:`None`.
+        """
         pass
 
     def bfs(self, node):
         """
-        Breadth-first search
+        Perform breadth-first search starting at a node.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         dq = deque([node])
         while dq:
@@ -29,8 +81,19 @@ class SimpleVisitor(object):
 
     def xbfs(self, node):
         """
-        Breadth-first search, except that 
-        leaf nodes are immediately visited.
+        Perform breadth-first search starting at a node,
+        except that leaf nodes are immediately visited.
+
+        Immediately visiting leaf nodes eliminates 
+        storing and retrieving leaf nodes, which is an expense
+        that may not be necessary if the search order can be
+        changed from the standard BFS.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         dq = deque([node])
         while dq:
@@ -45,7 +108,14 @@ class SimpleVisitor(object):
 
     def dfs_preorder(self, node):
         """
-        Depth-first search - preorder
+        Perform depth-first search starting at a node,
+        where nodes are visited before their children.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         dq = deque([node])
         while dq:
@@ -60,7 +130,14 @@ class SimpleVisitor(object):
 
     def dfs_postorder(self, node):
         """
-        Depth-first search - postorder
+        Perform depth-first search starting at a node,
+        where nodes are visited after their children.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         expanded = set()
         dq = deque([node])
@@ -77,7 +154,15 @@ class SimpleVisitor(object):
 
     def dfs_inorder(self, node):
         """
-        Depth-first search - inorder
+	    Perform depth-first search starting at a root node for
+	    a binary tree, where a node is visited after the left 
+        child and before the right child.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         expanded = set()
         dq = deque([node])
@@ -100,30 +185,103 @@ class SimpleVisitor(object):
 class ValueVisitor(object):
 
     def visit(self, node, values):  #pragma: no cover
+        """
+	    Visit a node in a tree and compute its value using
+        the values of its children.
+
+        This method should be over-written by a user
+        that is creating a sub-class.
+
+        Args:
+            node: a node in a tree
+            values: a list of values of this node's children
+
+        Returns:
+            The *value* for this node, which is computed using :attr:`values`
+        """
         pass
 
     def children(self, node):
+        """
+        Return the children for a node in a tree.
+        
+        This method has a default implementation, but
+        this implementation will likely be redefined
+        to reflect the tree data structures used for 
+        a specific application.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            A list of children of the specified node.
+        """ 
         return node.children
     
     def is_leaf(self, node):
+        """
+        Return :const:`True` if the node has no children.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            :const:`True` if this node has no children, and :const:`False`
+            otherwise
+        """
         return len(node.children) == 0
 
     def finalize(self, ans):    #pragma: no cover
-        pass
+        """
+        Return the "final value" of the search.
+
+        The default implementation returns the value of the
+        initial node (aka the root node), because
+        this visitor pattern computes and returns value for each
+        node to enable the computation of this value.
+
+        Returns:
+            The final value after the search.
+        """
+        return ans
 
     def visiting_potential_leaf(self, node):
         """ 
-        Visiting a potential leaf.
+        Visit a node and return its value if it is a leaf.
 
-        Return True if the node is not expanded.
+        Args:
+            node: a node in a tree
+
+        Returns:
+            A tuple: ``(flag, value)``.   If ``flag`` is False,
+            then the node is not a leaf and ``value`` is :const:`None`.  
+            Otherwise, ``value`` is used to compute the value of the 
+            parent node.
         """
+        #
+        # This default implementation relise on the is_leaf() and visit()
+        # methods.  While this provides a simple implementation,
+        # in practice a more efficient implementation would 
+        # ignore the is_leaf() method and implement this
+        # method separately for a specific application.
+        #
         if not self.is_leaf(node):
             return False, None
         return True, self.visit(node, None)
 
     def dfs_postorder_deque(self, node):
         """
-        Depth-first search - postorder with a dequeue
+        Perform depth-first search starting at a node,
+        where nodes are visited after their children.
+
+        This method uses a deque to manage the
+        set of nodes that need to be explored.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         flag, value = self.visiting_potential_leaf(node)
         if flag:
@@ -151,7 +309,17 @@ class ValueVisitor(object):
 
     def dfs_postorder_stack(self, node):
         """
-        Depth-first search - postorder with a stack
+        Perform depth-first search starting at a node,
+        where nodes are visited after their children.
+
+        This method uses a stack to manage the
+        set of nodes that need to be explored.
+
+        Args:
+            node: a node in a tree
+
+        Returns:
+            The value of the :func:`finalize` method.
         """
         flag, value = self.visiting_potential_leaf(node)
         if flag:
